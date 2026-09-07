e8a04b6598d6b8fe0aeb18327a07dd61e88a8862
24r21a6607-maker <24r21a6607@mlrit.ac.in>
2026-09-07 23:01:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableLambda
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-
 # --- 1. Define Tools ---
 @tool
 def search_movies(genre: str) -> str:
